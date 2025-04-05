c105c755155cfaf506744eee80f8c61bf354efcf
Anthony Liang <aliangdw@umich.edu>
2025-04-05 20:15:27 +0000

    update

--- robot_learning/utils/rollouts.py
+++ robot_learning/utils/rollouts.py
@@ -15,8 +15,7 @@ from omegaconf import DictConfig
 from PIL import Image, ImageDraw, ImageFont
 
 from robot_learning.data.dataclasses import Transition
-
-# from robot_learning.envs.utils import make_envs
+from robot_learning.envs.utils import make_envs
 from robot_learning.models.image_embedder import ImageEmbedder
 from robot_learning.utils.general_utils import to_numpy
 from robot_learning.utils.logger import log
